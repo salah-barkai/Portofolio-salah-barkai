a1f35f15b15a76761409a51de8ca538cbbe557f5
saladine ahmat barkai <93881972+salah-barkai@users.noreply.github.com>
2023-11-14 20:07:04 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const home = document.querySelector('#home');
+const reseau = document.querySelector('#reseau');
+const projet = document.querySelector('#projet');
+const toggle_on = document.querySelector('#btn-toggle-on');
+const toggle_off = document.querySelector('#btn-toggle-off');
+
+home.addEventListener('click', ()=>{
+    window.location.href= './index.html';
+});
+
+reseau.addEventListener('click', ()=>{
+    window.location.href='./reseau.html';
+});
+
+projet.addEventListener('click', ()=>{
+    window.location.href= './projet.html';
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    const body = document.body;
+
+    toggle_off.addEventListener('click', function () {
+        toggle_off.style.display = 'none';
+        toggle_on.style.display = 'block';
+        body.classList.toggle('dark-mode'); 
+        const elementsToToggleDarkMode = document.querySelectorAll('.projet-container, .img-container, .gran-img-contenaire, .container, .contenaire, .competences, .bar-bas, .image_language, .mes_reseaux');
+
+        elementsToToggleDarkMode.forEach(function (element) {
+            element.classList.toggle('dark-mode');
+        });
+    });
+
+    toggle_on.addEventListener('click', ()=>{
+        toggle_off.style.display = 'block';
+        toggle_on.style.display = 'none';
+        body.classList.remove('dark-mode'); // Utilisez remove au lieu de toggle pour s'assurer que le mode sombre est désactivé
+        const elementsToToggleDarkMode = document.querySelectorAll('.projet-container, .img-container, .gran-img-contenaire, .container, .contenaire, .competences, .bar-bas, .image_language, .mes_reseaux');
+
+        elementsToToggleDarkMode.forEach(function (element) {
+            element.classList.remove('dark-mode'); // Utilisez remove pour s'assurer que le mode sombre est désactivé
+        });
+    })
+});
